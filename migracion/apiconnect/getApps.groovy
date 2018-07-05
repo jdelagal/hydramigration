59078e9c9182730d0fc856bb8c637ef9f4c9fcc1
jdelagal <jdelagal@gmail.com>
2018-07-05 14:44:15 +0200

    getApps.

--- migracion/apiconnect/getApps.groovy
+++ migracion/apiconnect/getApps.groovy
@@ -1,16 +1,36 @@
-import groovy.json.*
+	import groovy.json.*
+    println "INICIO GET APPS"
+	/*parametros de entrada*/
+	//def consumerOwner = args[0]
+	def comsumerOwner = 'jdelagal@gmail.com'
+	//def consumerOwner = args[1]
+	def passwordOwner = '!n0r1t5@C'
+	//def consumerOwner = args[2]
+	def hostManager = 'apim'
+	//def consumerOwner = args[3]
+	def contextOrgProvider = 'factoriaustglobal.sb'
+	//def orgConsumer = args[4]
+	def orgConsumer='myorganization'
+	/*parametros de salida*/
+	def idOrgConsumidora=''
+	
+	GroovyShell shell = new GroovyShell()
+	def getOrgs = shell.parse(new File('getOrgs.groovy'))
+	def idConsumerOrg = getOrgs.getIdConsumerOrg()
 
-println "INICIO GET APPS"
 
-def cadenaAEjecutarApps = '''
-curl -k -v  -H "Content-Type: application/json" \
-			-H "Authorization:Basic amRlbGFnYWxAZ21haWwuY29tOiFuMHIxdDVAQw==" \
-			-H "X-IBM-APIManagement-Context: factoriaustglobal.sb" \
-			-X GET  https://apim/v1/portal/orgs/5ae9cdbce4b002e84d9e0d25/apps
-					  '''
-def jsonSlurper = new JsonSlurper()
-def jsonApps = jsonSlurper.parseText(cadenaAEjecutarApps.execute().text)
+	def tokenautorization = (comsumerOwner+':'+passwordOwner).bytes.encodeBase64().toString()
 
-println jsonApps
+	def cadenaAEjecutarApps = """
+		curl -k -v  -H "Content-Type: application/json" \
+				 -H "Authorization:Basic $tokenautorization"
+				 -H "X-IBM-APIManagement-Context: $contextOrgProvider" 
+				 -X GET  https://$hostManager/v1/portal/orgs/$idConsumerOrg/apps
+						  """
+
+	def jsonSlurper = new JsonSlurper()
+	def jsonApps = jsonSlurper.parseText(cadenaAEjecutarApps.execute().text)
+
+	println jsonApps
 
 println "FINAL GET APPS"
